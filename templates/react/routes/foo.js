45a0f77edf3d1b912c3e8aacbd4bd087a77250ca
Piotr Synowiec <psynowiec@gmail.com>
2017-05-15 20:54:09 +0200

    routing template and functionality finished

--- templates/react/routes/foo.js
+++ templates/react/routes/foo.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
 
+import { List as {{{titleUcFirst}}}List, Create as {{{titleUcFirst}}}Create, Update as {{{titleUcFirst}}}Update } from './components/{{{lc}}}/';
+
 export default (
     [
-        <Route path='/{{{lc}}}/' component={ {{{titleUcFirst}}} } exact={true} strict={true}/>
-
+        <Route path='/{{{lc}}}/' component={ {{{titleUcFirst}}}List } exact={true} strict={true}/>,
+        <Route path='/{{{lc}}}/create' component={ {{{titleUcFirst}}}Create } exact={true} />,
+        <Route path="/{{{lc}}}/edit/:id" component={ {{{titleUcFirst}}}Update } exact={true}/>
     ]
 );
