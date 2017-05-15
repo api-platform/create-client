48caa6f4a6c5d34e65694f116412dff59f6678c5
Piotr Synowiec <psynowiec@gmail.com>
2017-05-15 21:13:40 +0200

    console output only messaging generating code for resource

--- src/index.js
+++ src/index.js
@@ -30,22 +30,7 @@ parseHydraDocumentation(program.args[0]).then(api => {
       if (null === resourceToGenerate || nameLc === resourceToGenerate || titleLc === resourceToGenerate) {
         generator.generate(api, resource, program.args[1]);
 
-        const titleUcFirst = resource.title.charAt(0).toUpperCase() + resource.title.slice(1);
-
-        console.log('Code for the "%s" resource type has been generated!', resource.title);
-        console.log('Paste the following routes definitions in your application configuration:');
-        console.log(`
-  import ${titleLc} from './reducers/${titleLc}/';
-  import { List as ${titleUcFirst}List, Create as ${titleUcFirst}Create, Update as ${titleUcFirst}Update } from './components/${titleLc}/';
-  
-  // Add the reducer
-  combineReducers(${titleLc},{/* ... */}),
-
-  // Add the routes
-  <Route path="/${nameLc}/" component={${titleUcFirst}List} exact={true} strict={true}/>
-  <Route path="/${nameLc}/create" component={${titleUcFirst}Create} exact={true} />
-  <Route path="/${nameLc}/edit/:id" component={${titleUcFirst}Update} exact={true}/>
-`.green);
+        console.log('Code for the "%s" resource type has been generated!'.green, resource.title);
       }
     }
 }).catch((e) => {
