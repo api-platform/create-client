df6ba81e231138c677a69bb50698df4a7ffc4ab9
Piotr Synowiec <psynowiec@gmail.com>
2017-06-20 15:29:40 +0200

    Refactor of route names to fix issue with links. Now entrypoint "name" is component route name

--- templates/react/utils/itemToLinks.js
+++ templates/react/utils/itemToLinks.js
@@ -2,13 +2,15 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import {API_PATH} from '../api/_entrypoint';
 
-export default function (items) {
-  return Array.isArray(items) ? items.map(item => createLink(item)) : createLink(items);
+export default function(items) {
+  return Array.isArray(items)
+    ? items.map(item => createLink(item))
+    : createLink(items);
 }
 
 function createLink(item) {
-  if(item.includes(API_PATH)) {
-    const route = item.replace(API_PATH, '').split('/')[1].slice(0, -1);
+  if (typeof(item) === 'string' && item.includes(API_PATH) > 0) {
+    const route = item.replace(API_PATH, '').split('/')[1];
     return (
       <span key={item}>
         <Link to={`/${route}/show/${encodeURIComponent(item)}`}>
