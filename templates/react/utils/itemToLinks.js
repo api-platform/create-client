78a2e8f38fc4ba2f9528bd4ee20265c8f862c9af
Piotr Synowiec <psynowiec@gmail.com>
2017-06-26 09:29:20 +0200

    PR fixes #1 update

--- templates/react/utils/itemToLinks.js
+++ templates/react/utils/itemToLinks.js
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom';
 import {API_PATH} from '../api/_entrypoint';
 
 export default (items) => Array.isArray(items) ? items.map(item => createLink(item)) : createLink(items);
-gs
+
 function createLink(item) {
   if (typeof item === 'string' && item.includes(API_PATH) > 0) {
     const route = item.replace(API_PATH, '').split('/')[1];
