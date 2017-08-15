c1f932ddfd27200f3c12cc5dca89ddf2616088d9
Piotr Synowiec <psynowiec@gmail.com>
2017-08-15 10:28:20 +0200

    link label changed to /route/id

--- templates/react/utils/helpers.js
+++ templates/react/utils/helpers.js
@@ -3,17 +3,16 @@ import {Link} from 'react-router-dom';
 import {API_PATH} from '../api/_entrypoint';
 
 export function itemToLinks(items) {
-  return Array.isArray(items) ? items.map(item => createShortLink(item)) : createShortLink(items);
+  return Array.isArray(items) ? items.map(item => createLink(item)) : createLink(items);
 }
 
-function createShortLink(item) {
+function createLink(item) {
   if (typeof(item) === 'string' && item.includes(API_PATH) > 0) {
-    const label = item.split('/').splice(-1, 1);
     const route = item.replace(API_PATH, '').split('/')[0];
     return (
       <span key={item}>
         <Link to={`/${route}/show/${encodeURIComponent(item)}`}>
-          {label}
+          {item}
         </Link>
         <br/>
       </span>
