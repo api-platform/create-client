41dc722445113c1d24a622a1c16a4770c3a9bd5d
abluchet <abluchet@ds-restauration.com>
2017-09-14 10:39:12 +0200

    Allow multiple generators

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 #!/usr/bin/env node
 
 import 'isomorphic-fetch';
-import 'colors';
 import program from 'commander';
 import parseHydraDocumentation from 'api-doc-parser/lib/hydra/parseHydraDocumentation';
 import {version} from '../package.json';
-import ReactCrudGenerator from './ReactCrudGenerator';
+import generators from './generators';
 
 program
   .version(version)
@@ -13,13 +12,14 @@ program
   .usage('apiEntrypoint outputDirectory')
   .option('-r, --resource [resourceName]', 'Generate CRUD for the given resource')
   .option('-p, --hydra-prefix [hydraPrefix]', 'The hydra prefix used by the API', 'hydra:')
+  .option('-g, --generator [generator]', 'The generator to use, one of "react", "angular" etc.', 'react')
   .parse(process.argv);
 
 if (2 !== program.args.length) {
   program.help();
 }
 
-const generator = new ReactCrudGenerator(program.hydraPrefix);
+const generator = generators(program.generator)(program.hydraPrefix)
 const resourceToGenerate = program.resource ? program.resource.toLowerCase() : null;
 
 parseHydraDocumentation(program.args[0]).then(api => {
@@ -29,22 +29,7 @@ parseHydraDocumentation(program.args[0]).then(api => {
 
       if (null === resourceToGenerate || nameLc === resourceToGenerate || titleLc === resourceToGenerate) {
         generator.generate(api, resource, program.args[1]);
-
-        console.log('Code for the "%s" resource type has been generated!', resource.title);
-        console.log('Paste the following definitions in your application configuration:');
-        console.log(`
-// import reducers        
-import ${titleLc} from './reducers/${titleLc}/';
-
-//import routes
-import ${titleLc}Routes from './routes/${titleLc}';        
-        
-// Add the reducer
-combineReducers(${titleLc},{/* ... */}),
-  
-// Add routes to <Switch>
-{ ${titleLc}Routes }
-`.green);
+        generator.help(resource)
       }
     }
     generator.entrypoint(program.args[0], program.args[1]);
