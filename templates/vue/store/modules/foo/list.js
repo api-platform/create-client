74b1a8f9045541b5a76a94f3cf74b1e7cdba7e1f
Hippolyte Alain <alain.hippolyte@gmail.com>
2017-09-15 13:05:40 +0200

    Updated list store module for foo

--- templates/vue/store/modules/foo/list.js
+++ templates/vue/store/modules/foo/list.js
@@ -1,5 +1,11 @@
 import {{{ lc }}}Fetch from '../../api/{{{ lc }}}Fetch';
 
+const {{{ uc }}}_LIST_ERROR = '{{{ uc }}}_LIST_ERROR';
+const {{{ uc }}}_LIST_LOADING = '{{{ uc }}}_LIST_LOADING';
+const {{{ uc }}}_LIST_RESET = '{{{ uc }}}_LIST_RESET';
+const {{{ uc }}}_LIST_VIEW = '{{{ uc }}}_LIST_VIEW';
+const {{{ uc }}}_LIST_SUCCESS = '{{{ uc }}}_LIST_SUCCESS';
+
 const state = {
   loading: false,
   error: '',
@@ -10,42 +16,62 @@ const state = {
   }
 };
 
+function error(commit, error) {
+  return commit({{{ uc }}}_LIST_ERROR, error);
+}
+
+function loading(commit, loading) {
+  return commit({{{ uc }}}_LIST_LOADING, loading);
+}
+
+function success(commit, items) {
+  return commit({{{ uc }}}_LIST_SUCCESS, items);
+}
+
+function view(commit, items) {
+  return commit({{{ uc }}}_LIST_VIEW, items);
+}
+
+function reset(commit) {
+  return commit({{{ uc }}}_LIST_RESET);
+}
+
 const getters = {};
 
 const actions = {
     getItems({ commit, state }) {
-      commit('{{{ uc }}}_LIST_LOADING', true);
+      loading(commit, true);
 
       {{{ lc }}}Fetch('/{{{ name }}}', state.filters)
         .then(response => response.json())
         .then(data => {
           const items = data['{{{ hydraPrefix }}}member'];
-          const view = data[];
-          commit('{{{ uc }}}_LIST_LOADING', false);
-          commit('{{{ uc }}}_LIST_SUCCESS', { items });
-          commit('{{{ uc }}}_LIST_VIEW', { view });
+          const viewItems = data['{{{ hydraPrefix }}}view'];
+          loading(commit, false);
+          success(commit, items);
+          view(commit, viewItems);
         })
         .catch(e => {
-          commit('{{{ uc }}}_LIST_LOADING', false);
-          commit('{{{ uc }}}_LIST_ERROR', e.message);
+          loading(commit, false);
+          error(commit, e.message);
         });
     }
 };
 
 const mutations = {
-    ['{{{ uc }}}_LIST_ERROR'] (state, { error }) {
+    [{{{ uc }}}_LIST_ERROR] (state, { error }) {
       state.error = error;
     },
-    ['{{{ uc }}}_LIST_LOADING'] (state, { loading }) {
+    [{{{ uc }}}_LIST_LOADING] (state, { loading }) {
       state.loading = loading;
     },
-    ['{{{ uc }}}_LIST_RESET'] (state) {
+    [{{{ uc }}}_LIST_RESET] (state) {
       state.items = [];
     },
-    ['{{{ uc }}}_LIST_VIEW'] (state, { items }) {
+    [{{{ uc }}}_LIST_VIEW] (state, { items }) {
       state.view = items;
     },
-    ['{{{ uc }}}_LIST_SUCCESS'] (state, { items }) {
+    [{{{ uc }}}_LIST_SUCCESS] (state, { items }) {
       state.items = items;
     }
 };
