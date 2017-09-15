cec243844e9b03b932d1c9a2831460098ddea5e4
Hippolyte Alain <alain.hippolyte@gmail.com>
2017-09-15 15:13:36 +0200

    Added delete store module for foo

---
+++ templates/vue/store/modules/foo/delete.js
@@ -0,0 +1,60 @@
+import {{{ lc }}}Fetch from '../../api/{{{ lc }}}Fetch';
+
+const {{{ uc }}}_DELETE_ERROR = '{{{ uc }}}_DELETE_ERROR';
+const {{{ uc }}}_DELETE_LOADING = '{{{ uc }}}_DELETE_LOADING';
+const {{{ uc }}}_DELETE_SUCCESS = '{{{ uc }}}_DELETE_SUCCESS';
+
+const state = {
+  loading: false,
+  error: '',
+  deleted: null
+};
+
+function error(commit, error) {
+  return commit({{{ uc }}}_DELETE_ERROR, error);
+}
+
+function loading(commit, loading) {
+  return commit({{{ uc }}}_DELETE_LOADING, loading);
+}
+
+function success(commit, deleted) {
+  return commit({{{ uc }}}_DELETE_SUCCESS, deleted);
+}
+
+const getters = {};
+
+const actions = {
+  delete({ commit }) {
+    loading(commit, true);
+
+    return {{{ lc }}}Fetch(item['@id'], {method: 'DELETE'})
+      .then(() => {
+        loading(commit, false);
+        success(commit, item);
+      })
+      .catch(e => {
+        loading(commit, false);
+        error(commit, e.message);
+      });
+  }
+};
+
+const mutations = {
+    [{{{ uc }}}_DELETE_ERROR] (state, { error }) {
+      state.error = error;
+    },
+    [{{{ uc }}}_DELETE_LOADING] (state, { loading }) {
+      state.loading = loading;
+    },
+    [{{{ uc }}}_DELETE_SUCCESS] (state, { deleted }) {
+      state.deleted = deleted;
+    }
+};
+
+export default {
+    state,
+    getters,
+    actions,
+    mutations
+};
--- templates/vue/store/modules/foo/index.js
+++ templates/vue/store/modules/foo/index.js
@@ -1,15 +1,15 @@
-import {
-  list,
-  create,
-  update,
-  show
-} from './';
+import list from './list';
+import create from './create';
+import update from './update';
+import show from './show'
+import del from './delete';
 
 export default {
   modules: {
     list,
     create,
     update,
-    show
+    show,
+    del
   }
 }
