bfb71fb37f539364e2c363fd3dc5c88191f7f972
Hippolyte Alain <alain.hippolyte@gmail.com>
2017-09-22 15:47:16 +0200

    Added VueCrudGenerator.test

---
+++ src/generators/VueCrudGenerator.test.js
@@ -0,0 +1,53 @@
+import Api from 'api-doc-parser/lib/Api';
+import Resource from 'api-doc-parser/lib/Resource';
+import Field from 'api-doc-parser/lib/Field';
+import fs from 'fs';
+import tmp from 'tmp';
+import VueCrudGenerator from './VueCrudGenerator';
+
+
+test('Generate a Vue app', () => {
+  const generator = new VueCrudGenerator({hydraPrefix: 'hydra:', templateDirectory: `${__dirname}/../../templates`});
+  const tmpobj = tmp.dirSync({unsafeCleanup: true});
+
+  const fields = [new Field('bar', {
+    id: 'http://schema.org/url',
+    range: 'http://www.w3.org/2001/XMLSchema#string',
+    reference: null,
+    required: true,
+    description: 'An URL'
+  })];
+  const resource = new Resource('abc', 'http://example.com/foos', {
+    id: 'foo',
+    title: 'Foo',
+    readableFields: fields,
+    writableFields: fields
+  });
+  const api = new Api('http://example.com', {
+    title: 'My API',
+    resources: [resource]
+  });
+  generator.generate(api, resource, tmpobj.name);
+
+  expect(fs.existsSync(tmpobj.name+'/api/abcFetch.js'), true);
+
+  expect(fs.existsSync(tmpobj.name+'/components/abc/Create.vue'), true);
+  expect(fs.existsSync(tmpobj.name+'/components/abc/Form.vue'), true);
+  expect(fs.existsSync(tmpobj.name+'/components/abc/List.vue'), true);
+  expect(fs.existsSync(tmpobj.name+'/components/abc/Show.vue'), true);
+  expect(fs.existsSync(tmpobj.name+'/components/abc/Update.vue'), true);
+
+  expect(fs.existsSync(tmpobj.name+'/config/_entrypoint.js'), true);
+
+  expect(fs.existsSync(tmpobj.name+'/store/modules/abc/create.js'), true);
+  expect(fs.existsSync(tmpobj.name+'/store/modules/abc/delete.js'), true);
+  expect(fs.existsSync(tmpobj.name+'/store/modules/abc/index.js'), true);
+  expect(fs.existsSync(tmpobj.name+'/store/modules/abc/list.js'), true);
+  expect(fs.existsSync(tmpobj.name+'/store/modules/abc/mutation-types.js'), true);
+  expect(fs.existsSync(tmpobj.name+'/store/modules/abc/show.js'), true);
+  expect(fs.existsSync(tmpobj.name+'/store/modules/abc/update.js'), true);
+
+  expect(fs.existsSync(tmpobj.name+'/utils/fetch.js'), true);
+
+  tmpobj.removeCallback();
+});
