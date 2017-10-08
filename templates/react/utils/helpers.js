dedd8ddde5b6142c30a93fd63b55ce45a091888f
Kévin Dunglas <dunglas@gmail.com>
2017-10-08 13:51:50 +0200

    [react] Replace span by div in link lists (#48)

--- templates/react/utils/helpers.js
+++ templates/react/utils/helpers.js
@@ -8,14 +8,14 @@ export function itemToLinks(items) {
 
 function createLink(item) {
   if ('string' !== typeof(item) || !item.includes(API_PATH)) {
-    return <span key={item}>{item}<br/></span>;
+    return <div key={item}>{item}</div>;
   }
 
   const routeWithoutPrefix = item.replace(API_PATH, '');
   const splittedRoute = routeWithoutPrefix.split('/');
   const route = '/' === routeWithoutPrefix[0] ? splittedRoute[1] : splittedRoute[0];
 
-  return <span><Link key={item} to={`/${route}/show/${encodeURIComponent(item)}`}>{item}</Link><br/></span>;
+  return <div><Link key={item} to={`/${route}/show/${encodeURIComponent(item)}`}>{item}</Link></div>;
 }
 
 export function paginationRoute(item) {
