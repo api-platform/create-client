da1d0b4ddb5b372b470bd0db87d5d234d9d5d390
mgiraud <11818237+mgiraud@users.noreply.github.com>
2017-12-13 15:16:00 +0100

    Remove brackets for module import (#65)

--- src/generators/VueGenerator.js
+++ src/generators/VueGenerator.js
@@ -51,7 +51,7 @@ const router = new VueRouter({
 });
 
 // Add the modules in the store
-import { ${titleLc} } from './store/modules/${titleLc}/';
+import ${titleLc} from './store/modules/${titleLc}/';
 
 export const store = new Vuex.Store({
   // ...
