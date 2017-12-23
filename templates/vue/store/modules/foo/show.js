68533400d7c8a7913ed94ae28bfeba2881de4f80
David <david.annebicque@gmail.com>
2017-12-23 14:27:06 +0100

    Update show.js

--- templates/vue/store/modules/foo/show.js
+++ templates/vue/store/modules/foo/show.js
@@ -1,73 +1,74 @@
-import fetch from '../../../utils/fetch';
+import fetch from '../../../utils/fetch'
+
 import {
   {{{ uc }}}_SHOW_ERROR,
   {{{ uc }}}_SHOW_LOADING,
   {{{ uc }}}_SHOW_RETRIEVED_SUCCESS,
   {{{ uc }}}_SHOW_RESET
-} from './mutation-types';
+} from './mutation-types'
 
 const state = {
   loading: false,
   error: '',
   retrieved: null
-};
-
-function error(error) {
-  return {type: {{{ uc }}}_SHOW_ERROR, error};
 }
 
-function loading(loading) {
-  return {type: {{{ uc }}}_SHOW_LOADING, loading};
+function error (error) {
+  return {type: {{{ uc }}}_SHOW_ERROR, error}
 }
 
-function retrieved(retrieved) {
-  return {type: {{{ uc }}}_SHOW_RETRIEVED_SUCCESS, retrieved};
+function loading (loading) {
+  return {type: {{{ uc }}}_SHOW_LOADING, loading}
 }
 
-function reset() {
-  return {type: {{{ uc }}}_SHOW_RESET};
+function retrieved (retrieved) {
+  return {type: {{{ uc }}}_SHOW_RETRIEVED_SUCCESS, retrieved}
+}
+
+function reset () {
+  return {type: {{{ uc }}}_SHOW_RESET}
 }
 
 const getters = {
   error: state => state.error,
   loading: state => state.loading,
   item: state => state.retrieved
-};
+}
 
 const actions = {
-  retrieve({ commit }, id) {
-    commit(loading(true));
+  retrieve ({ commit }, id) {
+    commit(loading(true))
 
     return fetch(id)
       .then(response => response.json())
       .then(data => {
-        commit(loading(false));
-        commit(retrieved(data));
+        commit(loading(false))
+        commit(retrieved(data))
       })
       .catch(e => {
-        commit(loading(false));
-        commit(error(e.message));
+        commit(loading(false))
+        commit(error(e.message))
       });
   },
-  reset({ commit }) {
-    commit(reset());
+  reset ({ commit }) {
+    commit(reset())
   }
 };
 
 const mutations = {
-    [{{{ uc }}}_SHOW_ERROR] (state, payload) {
-      state.error = payload.error;
-    },
-    [{{{ uc }}}_SHOW_LOADING] (state, payload) {
-      state.loading = payload.loading;
-    },
-    [{{{ uc }}}_SHOW_RETRIEVED_SUCCESS] (state, payload) {
-      state.retrieved = payload.retrieved;
-    },
-    [{{{ uc }}}_SHOW_RESET] (state) {
-      state.retrieved = null;
-    }
-};
+  [{{{ uc }}}_SHOW_ERROR] (state, payload) {
+    state.error = payload.error
+  },
+  [{{{ uc }}}_SHOW_LOADING] (state, payload) {
+    state.loading = payload.loading
+  },
+  [{{{ uc }}}_SHOW_RETRIEVED_SUCCESS] (state, payload) {
+    state.retrieved = payload.retrieved
+  },
+  [{{{ uc }}}_SHOW_RESET] (state) {
+    state.retrieved = null
+  }
+}
 
 export default {
   namespaced: true,
@@ -75,4 +76,4 @@ export default {
   getters,
   actions,
   mutations
-};
+}
