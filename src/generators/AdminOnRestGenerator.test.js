0524261a5a170e05be1d30801f261e74d3b6e306
Kévin Dunglas <dunglas@gmail.com>
2018-05-29 15:16:14 +0200

    Configure Prettier. Adapt the Travis setup.

--- src/generators/AdminOnRestGenerator.test.js
+++ src/generators/AdminOnRestGenerator.test.js
@@ -1,46 +1,48 @@
-import Api from '@api-platform/api-doc-parser/lib/Api';
-import Resource from '@api-platform/api-doc-parser/lib/Resource';
-import Field from '@api-platform/api-doc-parser/lib/Field';
-import fs from 'fs';
-import tmp from 'tmp';
-import AdminOnRestGenerator from './AdminOnRestGenerator';
+import Api from "@api-platform/api-doc-parser/lib/Api";
+import Resource from "@api-platform/api-doc-parser/lib/Resource";
+import Field from "@api-platform/api-doc-parser/lib/Field";
+import fs from "fs";
+import tmp from "tmp";
+import AdminOnRestGenerator from "./AdminOnRestGenerator";
 
-test('Generate a Admin On Rest app', () => {
-  const generator = new AdminOnRestGenerator({hydraPrefix: 'hydra:', templateDirectory: `${__dirname}/../../templates`});
-  const tmpobj = tmp.dirSync({unsafeCleanup: true});
+test("Generate a Admin On Rest app", () => {
+  const generator = new AdminOnRestGenerator({
+    hydraPrefix: "hydra:",
+    templateDirectory: `${__dirname}/../../templates`
+  });
+  const tmpobj = tmp.dirSync({ unsafeCleanup: true });
 
-  const fields = [new Field('bar', {
-    id: 'http://schema.org/url',
-    range: 'http://www.w3.org/2001/XMLSchema#string',
-    reference: null,
-    required: true,
-    description: 'An URL'
-  })];
-  const resource = new Resource('abc', 'http://example.com/foos', {
-    id: 'abc',
-    title: 'abc',
+  const fields = [
+    new Field("bar", {
+      id: "http://schema.org/url",
+      range: "http://www.w3.org/2001/XMLSchema#string",
+      reference: null,
+      required: true,
+      description: "An URL"
+    })
+  ];
+  const resource = new Resource("abc", "http://example.com/foos", {
+    id: "abc",
+    title: "abc",
     readableFields: fields,
     writableFields: fields
   });
-  const api = new Api('http://example.com', {
-    entrypoint: 'http://example.com:8080',
-    title: 'My API',
+  const api = new Api("http://example.com", {
+    entrypoint: "http://example.com:8080",
+    title: "My API",
     resources: [resource]
   });
   generator.generate(api, resource, tmpobj.name);
 
   [
-    '/config/_entrypoint.js',
-    '/resources/abc.js',
-    '/resource-import.js',
-  ].forEach(file => expect(fs.existsSync(tmpobj.name+file)).toBe(true));
+    "/config/_entrypoint.js",
+    "/resources/abc.js",
+    "/resource-import.js"
+  ].forEach(file => expect(fs.existsSync(tmpobj.name + file)).toBe(true));
 
-  [
-    '/components/abc.js',
-    '/config/abc.js',
-  ].forEach(file => {
-    expect(fs.existsSync(tmpobj.name+file)).toBe(true)
-    expect(fs.readFileSync(tmpobj.name+file, 'utf8')).toMatch(/bar/);
+  ["/components/abc.js", "/config/abc.js"].forEach(file => {
+    expect(fs.existsSync(tmpobj.name + file)).toBe(true);
+    expect(fs.readFileSync(tmpobj.name + file, "utf8")).toMatch(/bar/);
   });
 
   tmpobj.removeCallback();
--- src/generators/ReactNativeGenerator.js
+++ src/generators/ReactNativeGenerator.js
@@ -1,5 +1,5 @@
-import chalk from 'chalk';
-import BaseGenerator from './BaseGenerator';
+import chalk from "chalk";
+import BaseGenerator from "./BaseGenerator";
 
 export default class extends BaseGenerator {
   constructor(params) {
@@ -7,52 +7,60 @@ export default class extends BaseGenerator {
 
     this.registerTemplates(`react-common/`, [
       // actions
-      'actions/foo/create.js',
-      'actions/foo/delete.js',
-      'actions/foo/list.js',
-      'actions/foo/update.js',
-      'actions/foo/show.js',
+      "actions/foo/create.js",
+      "actions/foo/delete.js",
+      "actions/foo/list.js",
+      "actions/foo/update.js",
+      "actions/foo/show.js",
 
       // utils
-      'utils/fetch.js',
+      "utils/fetch.js",
 
       // reducers
-      'reducers/foo/create.js',
-      'reducers/foo/delete.js',
-      'reducers/foo/index.js',
-      'reducers/foo/list.js',
-      'reducers/foo/update.js',
-      'reducers/foo/show.js',
+      "reducers/foo/create.js",
+      "reducers/foo/delete.js",
+      "reducers/foo/index.js",
+      "reducers/foo/list.js",
+      "reducers/foo/update.js",
+      "reducers/foo/show.js"
     ]);
 
     this.registerTemplates(`react/`, [
       // components
-      'components/foo/Create.js',
-      'components/foo/Form.js',
-      'components/foo/index.js',
-      'components/foo/List.js',
-      'components/foo/Update.js',
-      'components/foo/Show.js',
+      "components/foo/Create.js",
+      "components/foo/Form.js",
+      "components/foo/index.js",
+      "components/foo/List.js",
+      "components/foo/Update.js",
+      "components/foo/Show.js"
     ]);
   }
 
   help(resource) {
     const titleLc = resource.title.toLowerCase();
 
-    console.log('Code for the "%s" resource type has been generated!', resource.title);
-    console.log('Paste the following definitions in your application configuration:');
-    console.log(chalk.green(`
+    console.log(
+      'Code for the "%s" resource type has been generated!',
+      resource.title
+    );
+    console.log(
+      "Paste the following definitions in your application configuration:"
+    );
+    console.log(
+      chalk.green(`
 // import reducers
 import ${titleLc} from './reducers/${titleLc}/';
 
 // Add the reducer
 combineReducers(${titleLc},{/* ... */}),
-`));
+`)
+    );
   }
 
   generate(api, resource, dir) {
     const lc = resource.title.toLowerCase();
-    const titleUcFirst = resource.title.charAt(0).toUpperCase() + resource.title.slice(1);
+    const titleUcFirst =
+      resource.title.charAt(0).toUpperCase() + resource.title.slice(1);
 
     const context = {
       title: resource.title,
@@ -71,38 +79,42 @@ combineReducers(${titleLc},{/* ... */}),
       this.createDir(dir, false);
     }
 
-    for (let dir of [`${dir}/actions/${lc}`, `${dir}/components/${lc}`, `${dir}/reducers/${lc}`]) {
+    for (let dir of [
+      `${dir}/actions/${lc}`,
+      `${dir}/components/${lc}`,
+      `${dir}/reducers/${lc}`
+    ]) {
       this.createDir(dir);
     }
 
     for (let pattern of [
       // actions
-      'actions/%s/create.js',
-      'actions/%s/delete.js',
-      'actions/%s/list.js',
-      'actions/%s/update.js',
-      'actions/%s/show.js',
+      "actions/%s/create.js",
+      "actions/%s/delete.js",
+      "actions/%s/list.js",
+      "actions/%s/update.js",
+      "actions/%s/show.js",
 
       // components
-      'components/%s/Create.js',
-      'components/%s/Form.js',
-      'components/%s/index.js',
-      'components/%s/List.js',
-      'components/%s/Update.js',
-      'components/%s/Show.js',
+      "components/%s/Create.js",
+      "components/%s/Form.js",
+      "components/%s/index.js",
+      "components/%s/List.js",
+      "components/%s/Update.js",
+      "components/%s/Show.js",
 
       // reducers
-      'reducers/%s/create.js',
-      'reducers/%s/delete.js',
-      'reducers/%s/index.js',
-      'reducers/%s/list.js',
-      'reducers/%s/update.js',
-      'reducers/%s/show.js',
+      "reducers/%s/create.js",
+      "reducers/%s/delete.js",
+      "reducers/%s/index.js",
+      "reducers/%s/list.js",
+      "reducers/%s/update.js",
+      "reducers/%s/show.js"
     ]) {
       this.createFileFromPattern(pattern, dir, lc, context);
     }
 
-    this.createFile('utils/fetch.js', `${dir}/utils/fetch.js`, context, false);
-    this.createEntrypoint(api.entrypoint, `${dir}/config/_entrypoint.js`)
+    this.createFile("utils/fetch.js", `${dir}/utils/fetch.js`, context, false);
+    this.createEntrypoint(api.entrypoint, `${dir}/config/_entrypoint.js`);
   }
 }
