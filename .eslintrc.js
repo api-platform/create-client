193853a1bddced206691281bfa40a7955c3f4e3f
Kévin Dunglas <dunglas@gmail.com>
2018-05-29 18:35:53 +0200

    Fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,21 @@
 module.exports = {
-    'env': {
-        'es6': true,
-        'jest': true,
-        'node': true,
-    },
-    'parser': 'babel-eslint',
-    'parserOptions': {
-        'ecmaVersion': 7,
-        'sourceType': 'module'
-    },
-    'plugins': ['import'],
-    'extends': [
-        'plugin:prettier/recommended',
-        'eslint:recommended',
-    ],
-    'rules': {
-        'prettier/prettier': 'error',
-        'no-console': 0,
-    }
+  'env': {
+    'es6': true,
+    'jest': true,
+    'node': true,
+  },
+  'parser': 'babel-eslint',
+  'parserOptions': {
+    'ecmaVersion': 7,
+    'sourceType': 'module'
+  },
+  'plugins': ['import'],
+  'extends': [
+    'plugin:prettier/recommended',
+    'eslint:recommended',
+  ],
+  'rules': {
+    'prettier/prettier': 'error',
+    'no-console': 0,
+  }
 };
