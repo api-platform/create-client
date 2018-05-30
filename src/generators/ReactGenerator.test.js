a5bf833daf4d4651e35a85e80c4bc9b133b26344
Kévin Dunglas <dunglas@gmail.com>
2018-05-30 08:52:54 +0200

    Configure Prettier. Adapt the Travis setup. (#80)
    * Configure Prettier. Adapt the Travis setup.
    
    * Fix

--- src/generators/ReactGenerator.test.js
+++ src/generators/ReactGenerator.test.js
@@ -1,68 +1,71 @@
-import Api from '@api-platform/api-doc-parser/lib/Api';
-import Resource from '@api-platform/api-doc-parser/lib/Resource';
-import Field from '@api-platform/api-doc-parser/lib/Field';
-import fs from 'fs';
-import tmp from 'tmp';
-import ReactGenerator from './ReactGenerator';
+import Api from "@api-platform/api-doc-parser/lib/Api";
+import Resource from "@api-platform/api-doc-parser/lib/Resource";
+import Field from "@api-platform/api-doc-parser/lib/Field";
+import fs from "fs";
+import tmp from "tmp";
+import ReactGenerator from "./ReactGenerator";
 
-test('Generate a React app', () => {
-  const generator = new ReactGenerator({hydraPrefix: 'hydra:', templateDirectory: `${__dirname}/../../templates`});
-  const tmpobj = tmp.dirSync({unsafeCleanup: true});
+test("Generate a React app", () => {
+  const generator = new ReactGenerator({
+    hydraPrefix: "hydra:",
+    templateDirectory: `${__dirname}/../../templates`
+  });
+  const tmpobj = tmp.dirSync({ unsafeCleanup: true });
 
   const fields = [
-      new Field('bar', {
-        id: 'http://schema.org/url',
-        range: 'http://www.w3.org/2001/XMLSchema#string',
-        reference: null,
-        required: true,
-        description: 'An URL'
-      }),
+    new Field("bar", {
+      id: "http://schema.org/url",
+      range: "http://www.w3.org/2001/XMLSchema#string",
+      reference: null,
+      required: true,
+      description: "An URL"
+    })
   ];
-  const resource = new Resource('abc', 'http://example.com/foos', {
-    id: 'abc',
-    title: 'abc',
+  const resource = new Resource("abc", "http://example.com/foos", {
+    id: "abc",
+    title: "abc",
     readableFields: fields,
     writableFields: fields
   });
-  const api = new Api('http://example.com', {
-    entrypoint: 'http://example.com:8080',
-    title: 'My API',
+  const api = new Api("http://example.com", {
+    entrypoint: "http://example.com:8080",
+    title: "My API",
     resources: [resource]
   });
   generator.generate(api, resource, tmpobj.name);
 
   [
-    '/utils/fetch.js',
-    '/utils/helpers.js',
-    '/config/_entrypoint.js',
+    "/utils/fetch.js",
+    "/utils/helpers.js",
+    "/config/_entrypoint.js",
 
-    '/actions/abc/create.js',
-    '/actions/abc/delete.js',
-    '/actions/abc/list.js',
-    '/actions/abc/show.js',
-    '/actions/abc/update.js',
+    "/actions/abc/create.js",
+    "/actions/abc/delete.js",
+    "/actions/abc/list.js",
+    "/actions/abc/show.js",
+    "/actions/abc/update.js",
 
-    '/components/abc/index.js',
-    '/components/abc/Create.js',
-    '/components/abc/Update.js',
+    "/components/abc/index.js",
+    "/components/abc/Create.js",
+    "/components/abc/Update.js",
 
-    '/routes/abc.js',
+    "/routes/abc.js",
 
-    '/reducers/abc/create.js',
-    '/reducers/abc/delete.js',
-    '/reducers/abc/index.js',
-    '/reducers/abc/list.js',
-    '/reducers/abc/show.js',
-    '/reducers/abc/update.js',
-  ].forEach(file => expect(fs.existsSync(tmpobj.name+file)).toBe(true));
+    "/reducers/abc/create.js",
+    "/reducers/abc/delete.js",
+    "/reducers/abc/index.js",
+    "/reducers/abc/list.js",
+    "/reducers/abc/show.js",
+    "/reducers/abc/update.js"
+  ].forEach(file => expect(fs.existsSync(tmpobj.name + file)).toBe(true));
 
   [
-    '/components/abc/Form.js',
-    '/components/abc/List.js',
-    '/components/abc/Show.js',
+    "/components/abc/Form.js",
+    "/components/abc/List.js",
+    "/components/abc/Show.js"
   ].forEach(file => {
-    expect(fs.existsSync(tmpobj.name+file)).toBe(true);
-    expect(fs.readFileSync(tmpobj.name+file, 'utf8')).toMatch(/bar/);
+    expect(fs.existsSync(tmpobj.name + file)).toBe(true);
+    expect(fs.readFileSync(tmpobj.name + file, "utf8")).toMatch(/bar/);
   });
 
   tmpobj.removeCallback();
--- src/index.js
+++ src/index.js
@@ -1,48 +1,87 @@
 #!/usr/bin/env node
 
-import 'isomorphic-fetch';
-import program from 'commander';
-import parseHydraDocumentation from '@api-platform/api-doc-parser/lib/hydra/parseHydraDocumentation';
-import {version} from '../package.json';
-import generators from './generators';
+import "isomorphic-fetch";
+import program from "commander";
+import parseHydraDocumentation from "@api-platform/api-doc-parser/lib/hydra/parseHydraDocumentation";
+import { version } from "../package.json";
+import generators from "./generators";
 
 program
   .version(version)
-  .description('Generate a CRUD application built with React, Redux and React Router from an Hydra-enabled API')
-  .usage('entrypoint outputDirectory')
-  .option('-r, --resource [resourceName]', 'Generate CRUD for the given resource')
-  .option('-p, --hydra-prefix [hydraPrefix]', 'The hydra prefix used by the API', 'hydra:')
-  .option('-g, --generator [generator]', 'The generator to use, one of "react", "react-native", "vue", "admin-on-rest"', 'react')
-  .option('-t, --template-directory [templateDirectory]', 'The templates directory base to use. Final directory will be ${templateDirectory}/${generator}', `${__dirname}/../templates/`)
+  .description(
+    "Generate a CRUD application built with React, Redux and React Router from an Hydra-enabled API"
+  )
+  .usage("entrypoint outputDirectory")
+  .option(
+    "-r, --resource [resourceName]",
+    "Generate CRUD for the given resource"
+  )
+  .option(
+    "-p, --hydra-prefix [hydraPrefix]",
+    "The hydra prefix used by the API",
+    "hydra:"
+  )
+  .option(
+    "-g, --generator [generator]",
+    'The generator to use, one of "react", "react-native", "vue", "admin-on-rest"',
+    "react"
+  )
+  .option(
+    "-t, --template-directory [templateDirectory]",
+    "The templates directory base to use. Final directory will be ${templateDirectory}/${generator}",
+    `${__dirname}/../templates/`
+  )
   .parse(process.argv);
 
-if (2 !== program.args.length && (!process.env.API_PLATFORM_CLIENT_GENERATOR_ENTRYPOINT || !process.env.API_PLATFORM_CLIENT_GENERATOR_OUTPUT)) {
+if (
+  2 !== program.args.length &&
+  (!process.env.API_PLATFORM_CLIENT_GENERATOR_ENTRYPOINT ||
+    !process.env.API_PLATFORM_CLIENT_GENERATOR_OUTPUT)
+) {
   program.help();
 }
 
-const entrypoint = program.args[0] || process.env.API_PLATFORM_CLIENT_GENERATOR_ENTRYPOINT;
-const outputDirectory = program.args[1] || process.env.API_PLATFORM_CLIENT_GENERATOR_OUTPUT;
+const entrypoint =
+  program.args[0] || process.env.API_PLATFORM_CLIENT_GENERATOR_ENTRYPOINT;
+const outputDirectory =
+  program.args[1] || process.env.API_PLATFORM_CLIENT_GENERATOR_OUTPUT;
 
 const generator = generators(program.generator)({
   hydraPrefix: program.hydraPrefix,
   templateDirectory: program.templateDirectory
 });
-const resourceToGenerate = program.resource ? program.resource.toLowerCase() : null;
+const resourceToGenerate = program.resource
+  ? program.resource.toLowerCase()
+  : null;
 
-parseHydraDocumentation(entrypoint).then(ret => {
-  ret.api.resources.filter(({deprecated}) => !deprecated).forEach(resource => {
-    const nameLc = resource.name.toLowerCase();
-    const titleLc = resource.title.toLowerCase();
+parseHydraDocumentation(entrypoint)
+  .then(ret => {
+    ret.api.resources
+      .filter(({ deprecated }) => !deprecated)
+      .forEach(resource => {
+        const nameLc = resource.name.toLowerCase();
+        const titleLc = resource.title.toLowerCase();
 
-    if (null === resourceToGenerate || nameLc === resourceToGenerate || titleLc === resourceToGenerate) {
-      resource.fields = resource.fields.filter(({deprecated}) => !deprecated);
-      resource.readableFields = resource.readableFields.filter(({deprecated}) => !deprecated);
-      resource.writableFields = resource.writableFields.filter(({deprecated}) => !deprecated);
+        if (
+          null === resourceToGenerate ||
+          nameLc === resourceToGenerate ||
+          titleLc === resourceToGenerate
+        ) {
+          resource.fields = resource.fields.filter(
+            ({ deprecated }) => !deprecated
+          );
+          resource.readableFields = resource.readableFields.filter(
+            ({ deprecated }) => !deprecated
+          );
+          resource.writableFields = resource.writableFields.filter(
+            ({ deprecated }) => !deprecated
+          );
 
-      generator.generate(ret.api, resource, outputDirectory);
-      generator.help(resource)
-    }
+          generator.generate(ret.api, resource, outputDirectory);
+          generator.help(resource);
+        }
+      });
   })
-}).catch((e) => {
-  console.log(e);
-});
+  .catch(e => {
+    console.log(e);
+  });
