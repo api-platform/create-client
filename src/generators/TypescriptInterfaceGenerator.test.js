7aa830776ec6e9b219629f6c3963a99560ba0c0d
Luca Nardelli <lucanardelli91@gmail.com>
2019-05-21 23:15:55 +0200

    Handle resources with explicit id field

--- src/generators/TypescriptInterfaceGenerator.test.js
+++ src/generators/TypescriptInterfaceGenerator.test.js
@@ -54,10 +54,10 @@ test("Generate a typescript interface", () => {
 
 export interface Foo {
   '@id'?: string;
-  id?: string;
   foo: any;
   foobar?: FooBar;
   readonly bar: string;
+  id?: string;
 }
 `;
   expect(
@@ -106,9 +106,67 @@ test("Generate a typescript interface without references to other interfaces", (
 
   const res = `export interface Foo {
   '@id'?: string;
-  id?: string;
   foo: any;
   readonly bar: string;
+  id?: string;
+}
+`;
+  expect(
+    fs.readFileSync(tmpobj.name + "/interfaces/foo.ts").toString()
+  ).toEqual(res);
+
+  tmpobj.removeCallback();
+});
+
+test("Generate a typescript interface with an explicit id field in the readableFields", () => {
+  const generator = new TypescriptInterfaceGenerator({
+    templateDirectory: `${__dirname}/../../templates`
+  });
+  const tmpobj = tmp.dirSync({ unsafeCleanup: true });
+
+  const resource = new Resource("abc", "http://example.com/foos", {
+    id: "foo",
+    title: "Foo",
+    readableFields: [
+      new Field("bar", {
+        id: "http://schema.org/url",
+        range: "http://www.w3.org/2001/XMLSchema#string",
+        reference: null,
+        required: true,
+        description: "An URL"
+      }),
+      new Field("id", {
+        id: "http://schema.org/url",
+        range: "http://www.w3.org/2001/XMLSchema#string",
+        reference: null,
+        required: false,
+        description: "Id"
+      })
+    ],
+    writableFields: [
+      new Field("foo", {
+        id: "http://schema.org/url",
+        range: "http://www.w3.org/2001/XMLSchema#datetime",
+        reference: null,
+        required: true,
+        description: "An URL"
+      })
+    ]
+  });
+  const api = new Api("http://example.com", {
+    entrypoint: "http://example.com:8080",
+    title: "My API",
+    resources: [resource]
+  });
+  generator.generate(api, resource, tmpobj.name);
+
+  expect(fs.existsSync(tmpobj.name + "/interfaces/foo.ts")).toBe(true);
+
+  const res = `export interface Foo {
+  '@id'?: string;
+  foo: any;
+  readonly bar: string;
+  readonly id?: string;
 }
 `;
   expect(
