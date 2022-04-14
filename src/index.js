api-platform/create-client

commit b993f8248f4856f465f9db027b7f60dc3f5e5272

fix: format program option (#316)
index.js - options variable in parser was overshadowing options
variable from main

Co-authored-by: pom <>

src/index.js
@@ -79,17 +79,20 @@ async function main() {
     : null;
 
   const parser = (entrypointWithSlash) => {
-    const options = {};
-    if (options.username && options.password) {
+    const parserOptions = {};
+    if (parserOptions.username && parserOptions.password) {
       const encoded = Buffer.from(
-        `${options.username}:${options.password}`
+        `${parserOptions.username}:${parserOptions.password}`
       ).toString("base64");
-      options.headers = new Headers();
-      options.headers.set("Authorization", `Basic ${encoded}`);
+      parserOptions.headers = new Headers();
+      parserOptions.headers.set("Authorization", `Basic ${encoded}`);
     }
-    if (options.bearer) {
-      options.headers = new Headers();
-      options.headers.set("Authorization", `Bearer ${options.bearer}`);
+    if (parserOptions.bearer) {
+      parserOptions.headers = new Headers();
+      parserOptions.headers.set(
+        "Authorization",
+        `Bearer ${parserOptions.bearer}`
+      );
     }
     switch (options.format) {
       case "swagger": // deprecated
@@ -98,7 +101,7 @@ async function main() {
       case "openapi3":
         return parseOpenApi3Documentation(entrypointWithSlash);
       default:
-        return parseHydraDocumentation(entrypointWithSlash, options);
+        return parseHydraDocumentation(entrypointWithSlash, parserOptions);
     }
   };
 

commit 7db1671db8d9a39a66c4675543245215b652cd0e

fix: Don't ignore login data set via CLI params (regression from #316)
In attempting to fix an empty `options` object overshadowing the options set by the user via CLI, #316 mistakenly adjusted `options.username`, `options.password` and `options.bearer` to be attributes of the newly introduced `parserOptions`. They should, however, still refer to the `options` object that holds all the information previously set via CLI arguments.
I also added two comments to make the difference between the two sets of options more clear.

src/index.js
@@ -79,20 +79,19 @@ async function main() {
     : null;
 
   const parser = (entrypointWithSlash) => {
+    // parserOptions are used to set headers on the hydra-requests
     const parserOptions = {};
-    if (parserOptions.username && parserOptions.password) {
+    // options refers to the opts set via the CLI
+    if (options.username && options.password) {
       const encoded = Buffer.from(
-        `${parserOptions.username}:${parserOptions.password}`
+        `${options.username}:${options.password}`
       ).toString("base64");
       parserOptions.headers = new Headers();
       parserOptions.headers.set("Authorization", `Basic ${encoded}`);
     }
-    if (parserOptions.bearer) {
+    if (options.bearer) {
       parserOptions.headers = new Headers();
-      parserOptions.headers.set(
-        "Authorization",
-        `Bearer ${parserOptions.bearer}`
-      );
+      parserOptions.headers.set("Authorization", `Bearer ${options.bearer}`);
     }
     switch (options.format) {
       case "swagger": // deprecated