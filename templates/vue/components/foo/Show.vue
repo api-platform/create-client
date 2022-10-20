d2d58607dcc71c262ed56f9b0741fae972e60a76
Valentin Crochemore <valentin.crochemore@gmail.com>
2022-10-20 17:07:19 +0200

    Update Show component

--- templates/vue/components/foo/Show.vue
+++ templates/vue/components/foo/Show.vue
@@ -1,129 +1,125 @@
-<template>
-  <div>
-    <h1>Show Book \{{ item && item['@id'] }}</h1>
+<script lang="ts" setup>
+import { onBeforeUnmount } from "vue";
+import { useRoute, useRouter } from "vue-router";
+import { use{{{titleUcFirst}}}ShowStore } from "@/stores/{{{lc}}}/show";
+import { use{{{titleUcFirst}}}DeleteStore } from "@/stores/{{{lc}}}/delete";
+import { mercureSubscribe } from "../../utils/mercure";
+import { storeToRefs } from "pinia";
+import type { {{{titleUcFirst}}} } from "@/utils/types";
 
-    <div
-      v-if="isLoading"
-      class="alert alert-info"
-      role="status">Loading...</div>
-    <div
-      v-if="error"
-      class="alert alert-danger"
-      role="alert">
-      <span
-        class="fa fa-exclamation-triangle"
-        aria-hidden="true">\{{ error }}</span>
-    </div>
-    <div
-      v-if="deleteError"
-      class="alert alert-danger"
-      role="alert">
-      <span
-        class="fa fa-exclamation-triangle"
-        aria-hidden="true">\{{ deleteError }}</span>
-    </div>
-    <div
-      v-if="item"
-      class="table-responsive">
-      <table class="table table-striped table-hover">
-        <thead>
-          <tr>
-            <th>Field</th>
-            <th>Value</th>
-          </tr>
-        </thead>
-        <tbody>
-      {{#each fields}}
-          <tr>
-            <th scope="row">{{name}}</th>
-            <td>\{{ item['{{{name}}}'] }}</td>
-          </tr>
-      {{/each }}
-        </tbody>
-      </table>
-    </div>
+const route = useRoute();
+const router = useRouter();
 
-    <router-link
-      :to="{ name: '{{{titleUcFirst}}}List' }"
-      class="btn btn-primary">
-      Back to list
-    </router-link>
-    <router-link
-      v-if="item"
-      :to="{ name: '{{{titleUcFirst}}}Update', params: { id: item['@id'] } }"
-      class="btn btn-warning">
-      Edit
-    </router-link>
-    <button
-      class="btn btn-danger"
-      @click="del">Delete</button>
-  </div>
-</template>
+const {{{lc}}}ShowStore = use{{{titleUcFirst}}}ShowStore();
+const { retrieved: item, isLoading, error } = storeToRefs({{{lc}}}ShowStore);
 
-<script>
-import { mapActions } from 'vuex';
-import { mapFields } from 'vuex-map-fields';
-import ItemWatcher from '../../mixins/ItemWatcher';
-import * as types from '../../store/modules/{{{lc}}}/show/mutation_types';
-import * as delTypes from '../../store/modules/{{{lc}}}/delete/mutation_types';
+const mercureEl = (data: {{{titleUcFirst}}}) => {
+  if (Object.keys(data).length === 1) {
+    {{{lc}}}DeleteStore.setMercureDeleted(data);
+    return;
+  }
 
-export default {
-  mixins: [ItemWatcher],
-  computed: {
-    ...mapFields('{{{lc}}}/del', {
-      deleteError: 'error',
-      deleted: 'deleted',
-      mercureDeleted: 'mercureDeleted',
-    }),
-    ...mapFields('{{{lc}}}/show', {
-      error: 'error',
-      isLoading: 'isLoading',
-      item: 'retrieved',
-      hubUrl: 'hubUrl',
-    }),
-    itemUpdateMutation: () =>`{{{lc}}}/show/${types.{{{uc}}}_SHOW_SET_RETRIEVED}`,
-    itemMercureDeletedMutation: () => `{{{lc}}}/del/${delTypes.{{{uc}}}_DELETE_MERCURE_SET_DELETED}`,
-  },
-
-  watch: {
-    // eslint-disable-next-line object-shorthand,func-names
-    deleted: function(deleted) {
-      if (!deleted) {
-        return;
-      }
-
-      this.$router.push({ name: '{{{titleUcFirst}}}List' });
-    },
-    // eslint-disable-next-line object-shorthand,func-names
-    mercureDeleted: function(deleted) {
-      if (!deleted) {
-        return;
-      }
-
-      this.$router.push({ name: '{{{titleUcFirst}}}List' });
-    },
-  },
-
-  beforeDestroy () {
-    this.reset();
-  },
-
-  created () {
-    this.retrieve(decodeURIComponent(this.$route.params.id));
-  },
-
-  methods: {
-    ...mapActions({
-      deleteItem: '{{{lc}}}/del/del',
-      reset: '{{{lc}}}/show/reset',
-      retrieve: '{{{lc}}}/show/retrieve',
-    }),
-
-    del() {
-      if (window.confirm('Are you sure you want to delete this {{{lc}}}?')) {
-        this.deleteItem(this.item);
-      }
-    },
-  },
+  {{{lc}}}ShowStore.setRetrieved(data);
 };
+
+let mercureSub: EventSource | null = null;
+
+{{{lc}}}ShowStore.$subscribe((mutation, state) => {
+  if (!state.hubUrl) {
+    return;
+  }
+
+  if (mercureSub) {
+    mercureSub.close();
+  }
+
+  if (!state.retrieved) {
+    return;
+  }
+
+  mercureSub = mercureSubscribe(
+    state.hubUrl,
+    [state.retrieved["@id"] ?? ""],
+    mercureEl
+  );
+});
+
+await {{{lc}}}ShowStore.retrieve(decodeURIComponent(route.params.id as string));
+
+const {{{lc}}}DeleteStore = use{{{titleUcFirst}}}DeleteStore();
+const { error: deleteError, deleted } = storeToRefs({{{lc}}}DeleteStore);
+
+{{{lc}}}DeleteStore.$subscribe((mutation, state) => {
+  if (state.mercureDeleted) {
+    router.push({ name: "{{{titleUcFirst}}}List" });
+  }
+});
+
+async function deleteItem() {
+  if (!item.value) {
+    {{{lc}}}DeleteStore.setError("This item does not exist anymore");
+    return;
+  }
+
+  if (window.confirm("Are you sure you want to delete this {{{lc}}}?")) {
+    await {{{lc}}}DeleteStore.deleteItem(item.value);
+
+    if (deleted) {
+      router.push({ name: "{{{titleUcFirst}}}List" });
+    }
+  }
+}
+
+onBeforeUnmount(() => {
+  mercureSub?.close();
+  {{{lc}}}ShowStore.$reset();
+});
 </script>
+
+<template>
+  <h1>Show {{{titleUcFirst}}} \{{ item?.['@id'] }}</h1>
+
+  <div v-if="isLoading" class="alert alert-info" role="status">Loading...</div>
+  <div v-if="error" class="alert alert-danger" role="alert">
+    <i class="bi-exclamation-triangle" />
+    {{ error }}
+  </div>
+  <div v-if="deleteError" class="alert alert-danger" role="alert">
+    <<i class="bi-exclamation-triangle" />
+    {{ deleteError }}
+  </div>
+  
+  <div v-if="item" class="table-responsive">
+    <table class="table table-striped table-hover">
+      <thead>
+        <tr>
+          <th>Field</th>
+          <th>Value</th>
+        </tr>
+      </thead>
+      <tbody>
+    {{#each fields}}
+        <tr>
+          <th scope="row">{{name}}</th>
+          <td>\{{ item.{{{name}}} }}</td>
+        </tr>
+    {{/each }}
+      </tbody>
+    </table>
+  </div>
+
+  <router-link
+    :to="{ name: '{{{titleUcFirst}}}List' }"
+    class="btn btn-primary"
+  >
+    Back to list
+  </router-link>
+  <router-link
+    v-if="item"
+    :to="{ name: '{{{titleUcFirst}}}Update', params: { id: item['@id'] } }"
+    class="btn btn-warning"
+  >
+    Edit
+  </router-link>
+  <button class="btn btn-danger" @click="deleteItem">Delete</button>
+</template>
