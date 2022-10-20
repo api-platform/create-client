fa94cec6ba8ddbb30bbeb978eb8e8092ae3bc7eb
Valentin Crochemore <valentin.crochemore@gmail.com>
2022-10-20 15:52:27 +0200

    Update router from js to ts

--- templates/vue/router/foo.ts
+++ templates/vue/router/foo.ts
@@ -2,21 +2,21 @@ export default [
   {
     name: '{{{titleUcFirst}}}List',
     path: '/{{{name}}}/',
-    component: () => import('../components/{{{lc}}}/List.vue'),
+    component: () => import('../components/{{{lc}}}/ListView.vue'),
   },
   {
     name: '{{{titleUcFirst}}}Create',
     path: '/{{{name}}}/create',
-    component: () => import('../components/{{{lc}}}/Create.vue'),
+    component: () => import('../components/{{{lc}}}/CreateView.vue'),
   },
   {
     name: '{{{titleUcFirst}}}Update',
     path: '/{{{name}}}/edit/:id',
-    component: () => import('../components/{{{lc}}}/Update.vue'),
+    component: () => import('../components/{{{lc}}}/UpdateView.vue'),
   },
   {
     name: '{{{titleUcFirst}}}Show',
     path: '/{{{name}}}/show/:id',
-    component: () => import('../components/{{{lc}}}/Show.vue'),
+    component: () => import('../components/{{{lc}}}/ShowView.vue'),
   },
 ]
