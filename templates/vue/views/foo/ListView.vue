ff59a65390d486b4a59185d706077e026ebfe0e7
Valentin Crochemore <valentin.crochemore@gmail.com>
2022-10-20 15:54:57 +0200

    Add views to support Suspense

---
+++ templates/vue/views/foo/ListView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import {{{titleUcFirst}}}List from "@/components/{{{lc}}}/List.vue";
+</script>
+
+<template>
+  <Suspense>
+    <{{{titleUcFirst}}}List />
+
+    <template #fallback>Loading...</template>
+  </Suspense>
+</template>
