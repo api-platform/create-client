11934306da2d52827ed1a224fb735140c3435a37
Valentin Crochemore <valentin.crochemore@gmail.com>
2022-10-20 17:18:20 +0200

    Update Update component

--- templates/vue/components/foo/Update.vue
+++ templates/vue/components/foo/Update.vue
@@ -1,141 +1,154 @@
+<script lang="ts" setup>
+import { use{{{titleUcFirst}}}CreateStore } from "@/stores/{{{lc}}}/create";
+import { use{{{titleUcFirst}}}DeleteStore } from "@/stores/{{{lc}}}/delete";
+import { use{{{titleUcFirst}}}UpdateStore } from "@/stores/{{{lc}}}/update";
+import type { {{{titleUcFirst}}} } from "@/utils/types";
+import { storeToRefs } from "pinia";
+import { onBeforeUnmount } from "vue";
+import { useRoute, useRouter } from "vue-router";
+import {{{titleUcFirst}}}Form from "./{{{titleUcFirst}}}Form.vue";
+import { mercureSubscribe } from "../../utils/mercure";
+
+const route = useRoute();
+const router = useRouter();
+
+const {{{lc}}}UpdateStore = use{{{titleUcFirst}}}UpdateStore();
+const {
+  retrieved: item,
+  updated,
+  error,
+  isLoading,
+  violations,
+} = storeToRefs({{{lc}}}UpdateStore);
+
+const mercureEl = (data: {{{titleUcFirst}}}) => {
+  if (Object.keys(data).length === 1) {
+    {{{lc}}}DeleteStore.setMercureDeleted(data);
+    return;
+  }
+
+  {{{lc}}}UpdateStore.setUpdated(data);
+};
+
+let mercureSub: EventSource | null = null;
+
+{{{lc}}}UpdateStore.$subscribe((mutation, state) => {
+  if (!state.hubUrl) {
+    return;
+  }
+
+  if (mercureSub) {
+    mercureSub.close();
+  }
+
+  if (!state.retrieved) {
+    return;
+  }
+
+  mercureSub = mercureSubscribe(
+    state.hubUrl,
+    [state.retrieved["@id"] ?? ""],
+    mercureEl
+  );
+});
+
+await {{{lc}}}UpdateStore.retrieve(decodeURIComponent(route.params.id as string));
+
+function onSendForm(item: {{{titleUcFirst}}}) {
+  {{{lc}}}UpdateStore.update(item);
+}
+
+const {{{lc}}}CreateStore = use{{{titleUcFirst}}}CreateStore();
+const { created } = storeToRefs({{{lc}}}CreateStore);
+
+const {{{lc}}}DeleteStore = use{{{titleUcFirst}}}DeleteStore();
+const { error: deleteError, isLoading: deleteLoading } =
+  storeToRefs({{{lc}}}DeleteStore);
+
+{{{lc}}}DeleteStore.$subscribe((mutation, state) => {
+  if (state.mercureDeleted) {
+    router.push({ name: "{{{titleUcFirst}}}List" });
+  }
+});
+
+async function deleteItem() {
+  if (!item.value) {
+    {{{lc}}}UpdateStore.setError("No {{{lc}}} found. Please reload");
+    return;
+  }
+
+  if (window.confirm("Are you sure you want to delete this {{{lc}}}?")) {
+    await {{{lc}}}DeleteStore.deleteItem(item.value);
+
+    if ({{{lc}}}DeleteStore.deleted) {
+      router.push({ name: "{{{titleUcFirst}}}List" });
+    }
+  }
+}
+
+onBeforeUnmount(() => {
+  mercureSub?.close();
+  {{{lc}}}UpdateStore.$reset();
+  {{{lc}}}CreateStore.$reset();
+});
+</script>
+
 <template>
   <div>
-    <h1>Edit Book \{{ item && item['@id'] }}</h1>
+    <h1>Edit {{{titleUcFirst}}} \{{ item?.['@id'] }}</h1>
 
     <div
       v-if="created"
       class="alert alert-success"
-      role="status">\{{ created['@id'] }} created.</div>
+      role="status"
+    >
+      \{{ created['@id'] }} created.
+    </div>
     <div
       v-if="updated"
       class="alert alert-success"
-      role="status">\{{ updated['@id'] }} updated.</div>
+      role="status"
+    >
+      \{{ updated['@id'] }} updated.
+    </div>
     <div
       v-if="isLoading || deleteLoading"
       class="alert alert-info"
-      role="status">Loading...</div>
+      role="status"
+    >
+      Loading...
+    </div>
     <div
       v-if="error"
       class="alert alert-danger"
-      role="alert">
-      <span
-        class="fa fa-exclamation-triangle"
-        aria-hidden="true">\{{ error }}</span>
+      role="alert"
+    >
+      <i class="bi-exclamation-triangle" />
+      {{ error }}
     </div>
     <div
       v-if="deleteError"
       class="alert alert-danger"
-      role="alert">
-      <span
-        class="fa fa-exclamation-triangle"
-        aria-hidden="true">\{{ deleteError }}</span>
+      role="alert"
+    >
+      <i class="bi-exclamation-triangle" />
+      {{ deleteError }}
     </div>
 
     <{{{titleUcFirst}}}Form
       v-if="item"
-      :handle-submit="onSendForm"
       :values="item"
       :errors="violations"
-      :initial-values="item" />
+      @send-form="onSendForm"
+    />
 
     <router-link
       v-if="item"
       :to="{ name: '{{{titleUcFirst}}}List' }"
-      class="btn btn-primary">Back to list</router-link>
-    <button
-      class="btn btn-danger"
-      @click="del">Delete</button>
+      class="btn btn-primary"
+    >
+      Back to list
+    </router-link>
+    <button class="btn btn-danger" @click="deleteItem">Delete</button>
   </div>
 </template>
-
-<script>
-import { mapActions } from 'vuex';
-import { mapFields } from 'vuex-map-fields';
-import ItemWatcher from '../../mixins/ItemWatcher';
-import {{{titleUcFirst}}}Form from './Form.vue';
-import * as types from '../../store/modules/{{{lc}}}/update/mutation_types';
-import * as delTypes from '../../store/modules/{{{lc}}}/delete/mutation_types';
-
-export default {
-  mixins: [ItemWatcher],
-  components: {
-    {{{titleUcFirst}}}Form,
-  },
-
-  computed: {
-    ...mapFields('{{{lc}}}/del', {
-      deleteError: 'error',
-      deleteLoading: 'isLoading',
-      deleted: 'deleted',
-      mercureDeleted: 'mercureDeleted',
-    }),
-    ...mapFields('{{{lc}}}/create', {
-      created: 'created',
-    }),
-    ...mapFields('{{{lc}}}/update', {
-      isLoading: 'isLoading',
-      error: 'error',
-      item: 'retrieved',
-      hubUrl: 'hubUrl',
-      updated: 'updated',
-      violations: 'violations',
-    }),
-    itemUpdateMutation: () => `{{{lc}}}/update/${types.SET_RETRIEVED}`,
-    itemMercureDeletedMutation: () => `{{{lc}}}/del/${delTypes.{{{uc}}}_DELETE_MERCURE_SET_DELETED}`,
-  },
-
-  watch: {
-    // eslint-disable-next-line object-shorthand,func-names
-    deleted: function(deleted) {
-      if (!deleted) {
-        return;
-      }
-
-      this.$router.push({ name: '{{{titleUcFirst}}}List' });
-    },
-    // eslint-disable-next-line object-shorthand,func-names
-    mercureDeleted: function(deleted) {
-      if (!deleted) {
-        return;
-      }
-
-      this.$router.push({ name: '{{{titleUcFirst}}}List' });
-    },
-  },
-
-  beforeDestroy() {
-    this.reset();
-  },
-
-  created() {
-    this.retrieve(decodeURIComponent(this.$route.params.id));
-  },
-
-  methods: {
-    ...mapActions({
-      createReset: '{{{lc}}}/create/reset',
-      deleteItem: '{{{lc}}}/del/del',
-      delReset: '{{{lc}}}/del/reset',
-      retrieve: '{{{lc}}}/update/retrieve',
-      updateReset: '{{{lc}}}/update/reset',
-      update: '{{{lc}}}/update/update',
-      updateRetrieved: '{{{lc}}}/update/updateRetrieved',
-    }),
-
-    del() {
-      if (window.confirm('Are you sure you want to delete this {{{lc}}}?')) {
-        this.deleteItem(this.item);
-      }
-    },
-
-    reset() {
-      this.updateReset();
-      this.createReset();
-    },
-
-    onSendForm() {
-      this.update(this.item);
-    },
-  },
-};
-</script>
