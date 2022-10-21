636369cd158ee91495f43571b27e9e9c725dbfac
Valentin Crochemore <valentin.crochemore@gmail.com>
2022-10-21 14:31:16 +0200

    Fix component paths

--- templates/vue/router/foo.ts
+++ templates/vue/router/foo.ts
@@ -2,21 +2,21 @@ export default [
   {
     name: '{{{titleUcFirst}}}List',
     path: '/{{{name}}}/',
-    component: () => import('../components/{{{lc}}}/ListView.vue'),
+    component: () => import('@/views/{{{lc}}}/ListView.vue'),
   },
   {
     name: '{{{titleUcFirst}}}Create',
     path: '/{{{name}}}/create',
-    component: () => import('../components/{{{lc}}}/CreateView.vue'),
+    component: () => import('@/views/{{{lc}}}/CreateView.vue'),
   },
   {
     name: '{{{titleUcFirst}}}Update',
     path: '/{{{name}}}/edit/:id',
-    component: () => import('../components/{{{lc}}}/UpdateView.vue'),
+    component: () => import('@/views/{{{lc}}}/UpdateView.vue'),
   },
   {
     name: '{{{titleUcFirst}}}Show',
     path: '/{{{name}}}/show/:id',
-    component: () => import('../components/{{{lc}}}/ShowView.vue'),
+    component: () => import('@/views/{{{lc}}}/ShowView.vue'),
   },
 ]
