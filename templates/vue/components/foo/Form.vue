b37bd41ae1cabca2bbb02003b9077ccd0a75dbe0
Valentin Crochemore <valentin.crochemore@gmail.com>
2022-10-24 18:09:52 +0200

    Update form component

--- templates/vue/components/foo/Form.vue
+++ templates/vue/components/foo/Form.vue
@@ -1,115 +1,81 @@
+<script lang="ts" setup>
+{{#if hasManyRelations}}import FormRepeater from '@/components/common/FormRepeater.vue';{{/if}}
+import { formatDateInput } from "@/utils/date";
+import type { {{titleUcFirst}}, SubmissionErrors } from "@/utils/types";
+import { toRef } from "vue";
+
+const props = defineProps<{
+  values?: {{titleUcFirst}};
+  errors: SubmissionErrors | null;
+}>();
+
+const emit = defineEmits<{
+  (e: "sendForm", item: {{titleUcFirst}}): void;
+}>();
+
+const violations = toRef(props, "errors");
+
+let item: any = {};
+
+if (props.values) {
+  item = {
+    ...props.values,
+    {{#compare type "==" "dateTime" }}
+    publicationDate: formatDateInput(item.publicationDate),
+    {{/compare}}
+    {{#each fields}}
+    {{#if isEmbeddeds}}
+    {{name}}: props.value.{{name}}?.((item) => item["@id"] ?? "") ?? [],
+    {{else if embedded}}
+    {{name}}: props.value.{{name}}?.["@id"],
+    {{/if}}
+    {{/each}}
+  }
+}
+
+function emitSendForm() {
+  emit("sendForm", item);
+}
+</script>
+
 <template>
-  <form @submit.prevent="handleSubmit(item)">
-{{#each formFields}}
+  <form @submit.prevent="emitSendForm">
+  {{#each formFields}}
     <div class="form-group">
-      <label
-        for="{{{../lc}}}_{{{name}}}"
-        class="form-control-label">{{{name}}}</label>
-      {{#if reference}}
-        <select
-          v-model="item.{{{name}}}"
-          id="{{{../lc}}}_{{{name}}}"{{#unless maxCardinality}}
-          multiple{{/unless}}
-          class="form-control"
-        >
-          <option v-for="selectItem in {{{name}}}SelectItems"
-                  :key="selectItem['@id']"
-                  :value="selectItem['@id']"
-                  :selected="isSelected('{{{name}}}', selectItem['@id'])">\{{ selectItem.name }}
-          </option>
-        </select>
+      <label for="{{../lc}}_{{name}}" class="form-control-label">
+        {{name}}
+      </label>
+      {{#if isRelations}}
+      <FormRepeater :values="item.{{name}}" @update="(values) => item.{{name}} = values" />
       {{else}}
         <input
-          id="{{{../lc}}}_{{{name}}}"
-          v-model="item.{{{name}}}"
-          :class="['form-control', !isValid('{{{name}}}') ? 'is-invalid' : 'is-valid']"
-          type="{{{type}}}"{{#if step}}
-          step="{{{step}}}"{{/if}}{{#if required}}
-          required="required"{{/if}}
-          placeholder="{{{description}}}">
+          id="{{../lc}}_{{name}}"
+          v-model="item.{{name}}"
+          :class="[
+            'form-control', 
+            violations?.{{name}} ? 'is-invalid' : 'is-valid'
+          ]"
+          {{#compare type "==" "dateTime" }}
+          type="date"
+          {{/compare}}
+          {{#compare type "!=" "dateTime" }}
+          type="{{type}}"
+          {{/compare}}
+          {{#if step}}
+          step="{{step}}"
+          {{/if}}
+          {{#if required}}
+          required
+          {{/if}}
+          placeholder="{{description}}"
+        >
       {{/if}}
-      <div
-        v-if="!isValid('{{{name}}}')"
-        class="invalid-feedback">\{{ violations.{{{name}}} }}</div>
+      <div v-if="violations?.{{name}}" class="invalid-feedback">
+        \{{ violations.{{name}} }}
+      </div>
     </div>
-{{/each}}
+    {{/each}}
 
-    <button
-      type="submit"
-      class="btn btn-success">Submit</button>
+    <button type="submit" class="btn btn-success">Submit</button>
   </form>
 </template>
-
-<script>
-  import { find, get, isUndefined } from 'lodash';
-  import { mapActions } from 'vuex';
-
-  export default {
-  props: {
-    handleSubmit: {
-      type: Function,
-      required: true,
-    },
-
-    values: {
-      type: Object,
-      required: true,
-    },
-
-    errors: {
-      type: Object,
-      default: () => {},
-    },
-
-    initialValues: {
-      type: Object,
-      default: () => {},
-    }
-  },
-
-  mounted() {
-    {{#each formFields}}
-    {{#if reference}}
-    this.{{{name}}}GetSelectItems();
-    {{/if}}
-    {{/each}}
-  },
-
-  computed: {
-    {{#each formFields}}
-    {{#if reference}}
-    ...mapFields('{{{name}}}/list', {
-      '{{{name}}}SelectItems': 'selectItems',
-    }),
-    {{/if}}
-    {{/each}}
-
-    // eslint-disable-next-line
-    item() {
-      return this.initialValues || this.values;
-    },
-
-    violations() {
-      return this.errors || {};
-    },
-  },
-
-  methods: {
-    ...mapActions({
-      {{#each formFields}}
-      {{#if reference}}
-        {{{name}}}GetSelectItems: '{{{name}}}/list/getSelectItems',
-      {{/if}}
-      {{/each}}
-    }),
-
-    isSelected(collection, id) {
-      return find(this.item[collection], ['@id', id]) !== undefined;
-    },
-
-    isValid(key) {
-      return isUndefined(get(this.violations, key));
-    },
-  },
-};
-</script>
