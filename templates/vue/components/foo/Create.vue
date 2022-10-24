c0a66e0c38236d04abe20ccb4d223244c6fe02a9
Valentin Crochemore <valentin.crochemore@gmail.com>
2022-10-24 11:13:04 +0200

    Remove useless handlebars brackets

--- templates/vue/components/foo/Create.vue
+++ templates/vue/components/foo/Create.vue
@@ -1,41 +1,41 @@
 <script lang="ts" setup>
-import {{{titleUcFirst}}}Form from './Form.vue';
-import { use{{{titleUcFirst}}}CreateStore } from "@/stores/{{{lc}}}/create";
+import {{titleUcFirst}}Form from './Form.vue';
+import { use{{titleUcFirst}}CreateStore } from "@/stores/{{lc}}/create";
 import { useRouter } from "vue-router";
 import { storeToRefs } from "pinia";
-import type { {{{titleUcFirst}}} } from "@/utils/types";
+import type { {{titleUcFirst}} } from "@/utils/types";
 
 const router = useRouter();
 
-const {{{lc}}}CreateStore = use{{{titleUcFirst}}}CreateStore();
-const { isLoading, error, violations } = storeToRefs({{{lc}}}CreateStore);
+const {{lc}}CreateStore = use{{titleUcFirst}}CreateStore();
+const { isLoading, error, violations } = storeToRefs({{lc}}CreateStore);
 
-async function onSendForm(item: {{{titleUcFirst}}}) {
-  await {{{lc}}}CreateStore.create(item);
+async function onSendForm(item: {{titleUcFirst}}) {
+  await {{lc}}CreateStore.create(item);
 
-  if (!{{{lc}}}CreateStore.created) return;
+  if (!{{lc}}CreateStore.created) return;
 
   router.push({
-    name: "{{{titleUcFirst}}}Update",
-    params: { id: {{{lc}}}CreateStore.created["@id"] },
+    name: "{{titleUcFirst}}Update",
+    params: { id: {{lc}}CreateStore.created["@id"] },
   });
 }
 </script>
 
 <template>
-  <h1>Create {{{title}}}</h1>
+  <h1>Create {{title}}</h1>
 
   <div v-if="isLoading" class="alert alert-info" role="status">
     Loading...
   </div>
   <div v-if="error" class="alert alert-danger" role="alert">
     <i class="bi-exclamation-triangle" />
-    {{ error }}
+    \{{ error }}
   </div>
 
-  <{{{titleUcFirst}}}Form :errors="violations" @send-form="onSendForm" />
+  <{{titleUcFirst}}Form :errors="violations" @send-form="onSendForm" />
 
-  <router-link :to="{ name: '{{{titleUcFirst}}}List' }" class="btn btn-primary">
+  <router-link :to="{ name: '{{titleUcFirst}}List' }" class="btn btn-primary">
     Back to list
   </router-link>
 </template>
