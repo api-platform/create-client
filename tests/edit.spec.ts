6949d0f421839f4ad552e53bab9155059d2cb30a
Valentin Crochemore <valentin.crochemore@gmail.com>
2022-10-28 19:04:21 +0200

    Fix e2e tests

--- tests/edit.spec.ts
+++ tests/edit.spec.ts
@@ -1,10 +1,10 @@
 import { test as baseTest, expect } from '@playwright/test';
-import { locatorFixtures as fixtures, within } from '@playwright-testing-library/test/fixture.js';
+import { locatorFixtures as fixtures,  } from '@playwright-testing-library/test/fixture.js';
 import type { LocatorFixtures as TestingLibraryFixtures } from '@playwright-testing-library/test/fixture.js';
 
 const test = baseTest.extend<TestingLibraryFixtures>(fixtures)
 
-test('resource edit', async ({ page, queries: { getAllByRole, getByLabelText, getByRole, getByText, queryByRole, queryByText } }) => {
+test('resource edit', async ({ page, within, queries: { getAllByRole, getByLabelText, getByRole, getByText, queryByRole, queryByText } }) => {
   await page.goto('http://localhost:3000/books/');
 
   await expect(queryByText('Loading...')).not.toBeVisible();
