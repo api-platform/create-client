820c82315e51a93efc5a86d4cfbbf76c48836e45
Valentin Crochemore <valentin.crochemore@gmail.com>
2022-11-17 09:59:28 +0100

    Add pages

---
+++ templates/quasar/pages/foo/ShowPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <Suspense>
+    <{{titleUcFirst}}Show />
+
+    <template #fallback>Loading...</template>
+  </Suspense>
+</template>
+
+<script setup lang="ts">
+import {{titleUcFirst}}Show from 'components/{{lc}}/ShowComponent.vue';
+</script>
