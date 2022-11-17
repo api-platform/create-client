81287970aa4eb987e90f050ccf5815507c9a6f08
Valentin Crochemore <valentin.crochemore@gmail.com>
2022-11-17 09:59:28 +0100

    Add pages

---
+++ templates/quasar/pages/foo/CreatePage.vue
@@ -0,0 +1,11 @@
+<template>
+  <Suspense>
+    <{{titleUcFirst}}Create />
+
+    <template #fallback>Loading...</template>
+  </Suspense>
+</template>
+
+<script setup lang="ts">
+import {{titleUcFirst}}Create from 'components/{{lc}}/CreateComponent.vue';
+</script>
---
+++ templates/quasar/pages/foo/ListPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <Suspense>
+    <{{titleUcFirst}}List />
+
+    <template #fallback>Loading...</template>
+  </Suspense>
+</template>
+
+<script setup lang="ts">
+import {{titleUcFirst}}List from 'components/{{lc}}/ListComponent.vue';
+</script>
